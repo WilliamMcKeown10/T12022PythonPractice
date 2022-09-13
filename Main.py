fa82b4c843968260ec61190bee63660bb6abd8ff
William McKeown <825427@mystma.org>
2022-09-13 09:41:01 -0500

    3 functions

---
+++ Main.py
@@ -0,0 +1,17 @@
+def addTwoNumbers():
+    X = 100
+    Y = 10
+    print(X + Y)
+
+def myName():
+    print("William McKeown")
+
+def myFavoritThings():
+    print("1. Food 2. Water 3. Sports 4. Movie's 5. Games")
+
+def creditCardNumber():
+    print("7485-1258-99651")
+
+print(myName)
+print(myFavoritThings)
+print(creditCardNumber)
